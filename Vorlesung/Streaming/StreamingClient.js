65b9a034f5ee223eaaa34fdb042da121236b37dc
mgfeller <mgfeller@hsr.ch>
2015-06-10 13:59:27 +0200

    streaming

---
+++ Vorlesung/Streaming/StreamingClient.js
@@ -0,0 +1,15 @@
+var http = require("http");
+//1337, '127.0.0.1'
+var req = http.request( {hostname: "localhost", port: 1337, path : "/", method: "GET"}, function(res){
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+       console.log('BODY: ' + chunk.length);
+    });
+});
+
+req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+});
+req.end();
---
+++ Vorlesung/Streaming/StreamingServer.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var handler = function (req, res) {
+    if(req.url == "/"){
+        console.log("readFile")
+        res.writeHead(200, {'Content-Type': 'video'});
+        var stream = fs.createReadStream("./Wildlife.wmv" );
+        stream.pipe(res);
+    }
+};
+
+var server = http.createServer(handler);
+server.listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
