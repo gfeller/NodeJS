a0f55650ef02241b2b4efa87650f070a000547b4
mgfeller <mgfeller@hsr.ch>
2016-09-21 20:49:57 +0200

    encoding problem solved

--- Vorlesung/07_Streaming/StreamingClient.js
+++ Vorlesung/07_Streaming/StreamingClient.js
@@ -3,7 +3,7 @@ var http = require("http");
 var req = http.request( {hostname: "localhost", port: 1337, path : "/", method: "GET"}, function(res){
     console.log('STATUS: ' + res.statusCode);
     console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
+  //  res.setEncoding('utf8');
     res.on('data', function (chunk) {
        console.log('BODY: ' + chunk.length);
     });
