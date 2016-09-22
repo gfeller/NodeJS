594c74c37d42b94998737a9008d5f177425c02b8
mgfeller <mgfeller@hsr.ch>
2016-09-22 08:02:39 +0200

    updated demo

--- Vorlesung/06_Cycle/a.js
+++ Vorlesung/06_Cycle/a.js
@@ -1,4 +1,5 @@
+console.log("A1");
 var b = require("./b");
-console.log("A");
+console.log("A2");
 
 module.exports = {showObject : function(){  console.log("A", b)}};
--- Vorlesung/06_Cycle/b.js
+++ Vorlesung/06_Cycle/b.js
@@ -1,3 +1,4 @@
+console.log("B1");
 var a = require("./a");
-console.log("B");
+console.log("B2");
 module.exports = {showObject : function(){ console.log("B",a)}};
