473f34aa1222fa5513bdf5f8555d4cd175060b04
Michael Gfeller <mgfeller@hsr.ch>
2017-10-17 21:36:29 +0200

    linux fix

--- Vorlesung/03_Module/Main-ES6.js
+++ Vorlesung/03_Module/Main-ES6.js
@@ -1,12 +1,12 @@
 console.log("-----------------------");
-let counter5 = require("./counter3-ES6")();
+let counter5 = require("./Counter3-ES6")();
 console.log(counter5.get());
 console.log(counter5.count());
 console.log(counter5.count());
 console.log(counter5.count());
 
 console.log("-----------------------");
-let counter6 = new (require("./counter3-ES6")).Counter(20); /*Klammern wichtig - ansonsten 2 Zeilen*/
+let counter6 = new (require("./Counter3-ES6")).Counter(20); /*Klammern wichtig - ansonsten 2 Zeilen*/
 console.log(counter6.get());
 console.log(counter6.count());
 console.log(counter6.count());
