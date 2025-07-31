7916e6c4991a045872cb7167fcd649c750091d4b
Michael Gfeller <michael.gfeller@ost.ch>
2025-07-31 15:50:32 +0200

    update

--- Vorlesung/01_Callback/02b_Callback-Async-Await.js
+++ Vorlesung/01_Callback/02b_Callback-Async-Await.js
@@ -31,3 +31,37 @@
     }
 }());
 // console.log("END-FILE")
+
+/*
+
+(function () {
+    console.log("START");
+
+    loadFile("hello-world.pdf")
+        .then(function (result) {
+            console.log(result.message);
+            return loadFile("hello-world.docx");
+        })
+        .then(function (result) {
+            console.log(result);
+        })
+        .catch(function (e) {
+            console.log("ERROR", e.message);
+        })
+        .finally(function () {
+            console.log("ENDE");
+        });
+
+    function loadFile(file) {
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                if (file == null || !file.endsWith(".pdf")) {
+                    reject({ message: `File ${file} not found` });
+                } else {
+                    resolve({ message: `File ${file} found`, content: "Hello World" });
+                }
+            }, 2000);
+        });
+    }
+})();
+*/
